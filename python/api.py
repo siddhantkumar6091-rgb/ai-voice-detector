5d4b5a833af3495877bd88c287d9d9bc64795b36
Siddhant Kumar <siddhantkumar26@192.168.1.3>
2026-02-01 14:52:45 +0530

    enable cors for frontend

--- python/api.py
+++ python/api.py
@@ -9,6 +9,16 @@ from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
 from python.feature_extractor import extract_features
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app = FastAPI()
 
